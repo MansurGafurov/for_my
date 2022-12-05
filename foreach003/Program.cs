c887d85de772fb9f3a5562c41f861f5cdc7f9643
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-05 21:10:47 +0500

    цикл foreach 3  вариант

---
+++ foreach003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Перемножить каждый элемент на 2  
+int [] arr = {2, 4, 6, 8, 10, 12, 14};
+foreach (int index  in arr) 
+{
+    int num = 0;
+    num = index * 2;
+    Console.Write(num + ", ");
+}
+
