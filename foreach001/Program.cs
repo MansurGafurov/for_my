7a1ecc34b36aade0f2f41565504d41fffba49efe
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-05 20:12:37 +0500

    цикл foreach 1 вариант

---
+++ foreach001/Program.cs
@@ -0,0 +1,11 @@
+﻿// цикл foreach - проходит по каждому элементу 
+// и проделывает какую то работу (внутри цикла) с каждым элементом 
+
+int [] array = {1, 2, 3, 4, 5};
+int count = 0;
+foreach (int index  in array)
+{
+    Console.WriteLine($"Элемент {count} = {index}");
+    count++;
+}
+
