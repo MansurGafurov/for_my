86570f484366e50884a345b4ea19878a48b6b3f8
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-06 13:35:31 +0500

    Списки и массив

---
+++ список_массив/Program.cs
@@ -0,0 +1,16 @@
+﻿// Как добвить элементы из массива в лист?
+string[] arr = {"M", "A", "N", "S", "U", "R"};
+List<string> myList = new List<string> ();
+myList.AddRange(arr);
+myList.Add("G");
+myList.Add("A");
+myList.Add("F");
+myList.Add("U");
+myList.Add("R");
+myList.Add("O");
+myList.Add("V");
+
+foreach(string index in myList){
+    Console.Write(index);
+}
+
