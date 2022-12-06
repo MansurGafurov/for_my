4b587de4bed77e6e84de6b47b4e8bd087a20b457
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-06 20:34:44 +0500

    задачка для себя

---
+++ задачка_для_себя1/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задача. Дан массив с числами [ -1, 2 , 5, 8 ] ,  K = 7.  Напиши алгоритм, который найдет два  
+//  числа в сумме дающие К?
+
+int [] arr = {-1, 2, 5, 8};
+//List<int> myList = new List<int> ();
+int K = 7;
+for(int i = 0; i < arr.Length; i++) {
+    int a = arr[i];
+    for(int y = 0; y < arr.Length ; y++)
+    {
+        if ((a + arr[y]) == K) {
+            //myList.Add(a);
+            //myList.Add(arr[y]); 
+            Console.WriteLine($"[ {a} & {arr[y]} ]");
+        }
+        
+    }
+}
+
+//foreach(int index in myList){
+ //   Console.Write(index + ", ");
+//}
